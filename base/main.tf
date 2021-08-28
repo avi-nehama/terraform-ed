a17a3551a49775b4acb998381ad7e9f80384872b
Avi Nehama <avi.nehama@gmail.com>
2021-08-28 12:29:49 +0000

    refactors ovoc as a module

--- base/main.tf
+++ base/main.tf
@@ -24,54 +24,25 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "internal_subnet"
+resource "azurerm_subnet" "mng_subnet" {
+  name                 = "mng_subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "ovoc-nic" {
-  name                = "ovoc-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+module "ovoc" {
+  source = "../modules/ovoc"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  env_name = var.customer_name
+  location = var.location
+  subnet_id = azurerm_subnet.mng_subnet.id
 }
 
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                             = "ovoc-${var.customer_name}"
-  location                         = "${azurerm_resource_group.rg.location}"
-  resource_group_name              = "${azurerm_resource_group.rg.name}"
-  size                             = var.ovoc_vm_instance_type
-  admin_username                   = "ovocadmin"
-  admin_password                   = "P@$$w0rd1234!" 
-  network_interface_ids            = [azurerm_network_interface.ovoc-nic.id]
+# ---\/-----
+# 2*SBC
+# Load-balancer
+# Subnet
+# ---/\-----  
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
- 
-    # publisher = "MicrosoftWindowsServer"
-    # offer     = "WindowsServer"
-    # sku       = "2016-Datacenter"
-    # version   = "latest"
-
-   publisher = "audiocodes"
-   offer     = "audcovoc"
-   sku       = "acovoce4azure"
-   version   = "latest"
-
-  }
-}
-
-output "ovoc_private_ip" {
-  value = azurerm_network_interface.ovoc-nic.private_ip_address
-}
+# NSG - open ports
