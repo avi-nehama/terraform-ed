2a63240cf95036f375495a134d8b1ad78c42e53a
Avi Nehama <avi.nehama@gmail.com>
2021-09-05 13:17:48 +0000

    adds outputs

---
+++ base/outputs.tf
@@ -0,0 +1,11 @@
+output "ump_private_ip" {
+  value = module.ump.ump_private_ip
+}
+
+output "ovoc_private_ip" {
+  value = module.ovoc.ovoc_private_ip
+}
+
+
+
+
---
+++ modules/ump/outputs.tf
@@ -0,0 +1,4 @@
+output "ump_private_ip" {
+  value = azurerm_windows_virtual_machine.ump_vm.private_ip_address
+}
+
