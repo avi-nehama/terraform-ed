b0b9b607ff78559a3f94f0e7978f661a9c925c51
Avi Nehama <avi.nehama@gmail.com>
2021-09-05 13:05:12 +0000

    changes ump to a windows VM

--- modules/ump/main.tf
+++ modules/ump/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_network_interface" "ump_nic" {
-  name                = "ump_nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
+  name                            = "ump_nic"
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  enable_accelerated_networking   = true
   ip_configuration {
     name                          = "internal"
     subnet_id                     = var.subnet_id
@@ -11,22 +11,16 @@ resource "azurerm_network_interface" "ump_nic" {
 }
 
 
-resource "azurerm_linux_virtual_machine" "ump_vm" {
+resource "azurerm_windows_virtual_machine" "ump_vm" {
   name                            = "${var.resource_group_name}-ump-vm"
   location                        = var.location
   resource_group_name             = var.resource_group_name
   size                            = var.ump_instance_type
-  disable_password_authentication = false
   admin_username                  = var.ump_admin_username
   admin_password                  = var.ump_admin_password
   network_interface_ids           = [
     azurerm_network_interface.ump_nic.id
   ]
-
-  # admin_ssh_key {
-  #   username   = "adminuser"
-  #   public_key = file("~/.ssh/id_rsa.pub")
-  # }
 
   os_disk {
     caching              = "ReadWrite"
@@ -34,9 +28,9 @@ resource "azurerm_linux_virtual_machine" "ump_vm" {
   }
 
   source_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "8_2"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 }
